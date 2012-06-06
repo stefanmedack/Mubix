e111d53710a125e87f171c201dcba79ab069a6f6
Zonic <zonic03@web.de>
2012-06-06 15:05:05 +0200

    init

---
+++ src/de/tub/mubix/main/Cube.java
@@ -0,0 +1,362 @@
+package de.tub.mubix.main;
+
+import processing.core.PApplet;
+import processing.core.PVector;
+
+public class Cube {
+	PApplet parent;
+
+	final int LENGTH = 60; // size of cube
+	final int DEG = 3; // rotation speed (degrees per frame)
+
+	public int clock = 0;
+	public int faceTurn = 0;
+
+	PVector[][][] squareMatrix;
+	public int[][] colorTable = { { 255, 0, 0 }, { 255, 127, 0 },
+			{ 0, 0, 255 }, { 0, 255, 0 }, { 255, 255, 0 }, { 255, 255, 255 } };
+
+	/*
+	 * for scrambling from identity state rather than reading from camera. If
+	 * change into camera reading mode,this colorTable will be a list contains
+	 * 54 triples of color for each square, and initialized by a methodwhich
+	 * controls camera
+	 */
+
+	public Cube(PApplet parent) {// The points for each square on the face
+		this.parent = parent;
+
+		squareMatrix = new PVector[6][9][4];
+		for (int i = 0; i < 3; i++)
+			for (int j = 0; j < 3; j++) {
+				squareMatrix[0][i * 3 + j][0] = new PVector(
+						(i - 1.5f) * LENGTH, (j - 1.5f) * LENGTH, 1.5f * LENGTH);
+				squareMatrix[0][i * 3 + j][1] = new PVector(
+						(i - 1.5f) * LENGTH, (j - 1.5f + 1) * LENGTH,
+						1.5f * LENGTH);
+				squareMatrix[0][i * 3 + j][2] = new PVector((i - 1.5f + 1)
+						* LENGTH, (j - 1.5f + 1) * LENGTH, 1.5f * LENGTH);
+				squareMatrix[0][i * 3 + j][3] = new PVector((i - 1.5f + 1)
+						* LENGTH, (j - 1.5f) * LENGTH, 1.5f * LENGTH);
+			}
+		for (int i = 0; i < 3; i++)
+			for (int j = 0; j < 3; j++) {
+				squareMatrix[1][i * 3 + j][0] = new PVector(
+						(i - 1.5f) * LENGTH, (j - 1.5f) * LENGTH, -1.5f
+								* LENGTH);
+				squareMatrix[1][i * 3 + j][1] = new PVector(
+						(i - 1.5f) * LENGTH, (j - 1.5f + 1) * LENGTH, -1.5f
+								* LENGTH);
+				squareMatrix[1][i * 3 + j][2] = new PVector((i - 1.5f + 1)
+						* LENGTH, (j - 1.5f + 1) * LENGTH, -1.5f * LENGTH);
+				squareMatrix[1][i * 3 + j][3] = new PVector((i - 1.5f + 1)
+						* LENGTH, (j - 1.5f) * LENGTH, -1.5f * LENGTH);
+			}
+		for (int i = 0; i < 3; i++)
+			for (int j = 0; j < 3; j++) {
+				squareMatrix[2][i * 3 + j][0] = new PVector(
+						(i - 1.5f) * LENGTH, 1.5f * LENGTH, (j - 1.5f) * LENGTH);
+				squareMatrix[2][i * 3 + j][1] = new PVector(
+						(i - 1.5f) * LENGTH, 1.5f * LENGTH, (j - 1.5f + 1)
+								* LENGTH);
+				squareMatrix[2][i * 3 + j][2] = new PVector((i - 1.5f + 1)
+						* LENGTH, 1.5f * LENGTH, (j - 1.5f + 1) * LENGTH);
+				squareMatrix[2][i * 3 + j][3] = new PVector((i - 1.5f + 1)
+						* LENGTH, 1.5f * LENGTH, (j - 1.5f) * LENGTH);
+			}
+		for (int i = 0; i < 3; i++)
+			for (int j = 0; j < 3; j++) {
+				squareMatrix[3][i * 3 + j][0] = new PVector(
+						(i - 1.5f) * LENGTH, -1.5f * LENGTH, (j - 1.5f)
+								* LENGTH);
+				squareMatrix[3][i * 3 + j][1] = new PVector(
+						(i - 1.5f) * LENGTH, -1.5f * LENGTH, (j - 1.5f + 1)
+								* LENGTH);
+				squareMatrix[3][i * 3 + j][2] = new PVector((i - 1.5f + 1)
+						* LENGTH, -1.5f * LENGTH, (j - 1.5f + 1) * LENGTH);
+				squareMatrix[3][i * 3 + j][3] = new PVector((i - 1.5f + 1)
+						* LENGTH, -1.5f * LENGTH, (j - 1.5f) * LENGTH);
+			}
+		for (int i = 0; i < 3; i++)
+			for (int j = 0; j < 3; j++) {
+				squareMatrix[4][i * 3 + j][0] = new PVector(1.5f * LENGTH,
+						(i - 1.5f) * LENGTH, (j - 1.5f) * LENGTH);
+				squareMatrix[4][i * 3 + j][1] = new PVector(1.5f * LENGTH,
+						(i - 1.5f) * LENGTH, (j - 1.5f + 1) * LENGTH);
+				squareMatrix[4][i * 3 + j][2] = new PVector(1.5f * LENGTH,
+						(i - 1.5f + 1) * LENGTH, (j - 1.5f + 1) * LENGTH);
+				squareMatrix[4][i * 3 + j][3] = new PVector(1.5f * LENGTH,
+						(i - 1.5f + 1) * LENGTH, (j - 1.5f) * LENGTH);
+			}
+		for (int i = 0; i < 3; i++)
+			for (int j = 0; j < 3; j++) {
+				squareMatrix[5][i * 3 + j][0] = new PVector(-1.5f * LENGTH,
+						(i - 1.5f) * LENGTH, (j - 1.5f) * LENGTH);
+				squareMatrix[5][i * 3 + j][1] = new PVector(-1.5f * LENGTH,
+						(i - 1.5f) * LENGTH, (j - 1.5f + 1) * LENGTH);
+				squareMatrix[5][i * 3 + j][2] = new PVector(-1.5f * LENGTH,
+						(i - 1.5f + 1) * LENGTH, (j - 1.5f + 1) * LENGTH);
+				squareMatrix[5][i * 3 + j][3] = new PVector(-1.5f * LENGTH,
+						(i - 1.5f + 1) * LENGTH, (j - 1.5f) * LENGTH);
+			}
+	}
+
+	@SuppressWarnings("static-access")
+	public void display() {
+		for (int i = 0; i < 6; i++) {
+			parent.fill(colorTable[i][0], colorTable[i][1], colorTable[i][2]);
+			// If the surface colors are read from camera, the fill() should be
+			// insert in the Shape module opacity property doesn't seem to work
+			// on well with fill(), it'll be confusing when alpha applied.
+			for (int j = 0; j < 9; j++) {
+				parent.beginShape(parent.QUAD);
+				parent.vertex(squareMatrix[i][j][0].x, squareMatrix[i][j][0].y,
+						squareMatrix[i][j][0].z);
+				parent.vertex(squareMatrix[i][j][1].x, squareMatrix[i][j][1].y,
+						squareMatrix[i][j][1].z);
+				parent.vertex(squareMatrix[i][j][2].x, squareMatrix[i][j][2].y,
+						squareMatrix[i][j][2].z);
+				parent.vertex(squareMatrix[i][j][3].x, squareMatrix[i][j][3].y,
+						squareMatrix[i][j][3].z);
+				parent.endShape();
+			}
+		}
+	}
+
+	@SuppressWarnings("static-access")
+	public void rTransform(PVector p, int axis, int dir) {// Simple rotation
+		// transform, dir can be
+		// positive or negative
+		float temp;
+		if (axis == 1) {
+			temp = p.y;
+			p.y = p.y * parent.cos(parent.radians(dir * DEG)) - p.z
+					* parent.sin(parent.radians(dir * DEG));
+			p.z = temp * parent.sin(parent.radians(dir * DEG)) + p.z
+					* parent.cos(parent.radians(dir * DEG));
+		} else if (axis == 2) {
+			temp = p.x;
+			p.x = p.x * parent.cos(parent.radians(dir * DEG)) - p.z
+					* parent.sin(parent.radians(dir * DEG));
+			p.z = temp * parent.sin(parent.radians(dir * DEG)) + p.z
+					* parent.cos(parent.radians(dir * DEG));
+		} else {
+			temp = p.x;
+			p.x = p.x * parent.cos(parent.radians(dir * DEG)) - p.y
+					* parent.sin(parent.radians(dir * DEG));
+			p.y = temp * parent.sin(parent.radians(dir * DEG)) + p.y
+					* parent.cos(parent.radians(dir * DEG));
+		}
+	}
+
+	public void rightTwist(int rdir) {
+		for (int i = 0; i < 6; i++)
+			for (int j = 0; j < 9; j++)
+				if ((squareMatrix[i][j][0].x >= 1.5 * LENGTH - 1)
+						|| (squareMatrix[i][j][1].x >= 1.5 * LENGTH - 1)
+						|| (squareMatrix[i][j][2].x >= 1.5 * LENGTH - 1)
+						|| (squareMatrix[i][j][3].x >= 1.5 * LENGTH - 1)) {
+					rTransform(squareMatrix[i][j][0], 1, rdir);
+					rTransform(squareMatrix[i][j][1], 1, rdir);
+					rTransform(squareMatrix[i][j][2], 1, rdir);
+					rTransform(squareMatrix[i][j][3], 1, rdir);
+				}
+	}
+
+	public void leftTwist(int rdir) {
+		for (int i = 0; i < 6; i++)
+			for (int j = 0; j < 9; j++)
+				if ((squareMatrix[i][j][0].x <= -(1.5 * LENGTH - 1))
+						|| (squareMatrix[i][j][1].x <= -(1.5 * LENGTH - 1))
+						|| (squareMatrix[i][j][2].x <= -(1.5 * LENGTH - 1))
+						|| (squareMatrix[i][j][3].x <= -(1.5 * LENGTH - 1))) {
+					rTransform(squareMatrix[i][j][0], 1, rdir);
+					rTransform(squareMatrix[i][j][1], 1, rdir);
+					rTransform(squareMatrix[i][j][2], 1, rdir);
+					rTransform(squareMatrix[i][j][3], 1, rdir);
+				}
+	}
+
+	public void bottomTwist(int rdir) {
+		for (int i = 0; i < 6; i++)
+			for (int j = 0; j < 9; j++)
+				if ((squareMatrix[i][j][0].y >= 1.5 * LENGTH - 1)
+						|| (squareMatrix[i][j][1].y >= 1.5 * LENGTH - 1)
+						|| (squareMatrix[i][j][2].y >= 1.5 * LENGTH - 1)
+						|| (squareMatrix[i][j][3].y >= 1.5 * LENGTH - 1)) {
+					rTransform(squareMatrix[i][j][0], 2, rdir);
+					rTransform(squareMatrix[i][j][1], 2, rdir);
+					rTransform(squareMatrix[i][j][2], 2, rdir);
+					rTransform(squareMatrix[i][j][3], 2, rdir);
+				}
+	}
+
+	public void upTwist(int rdir) {
+		for (int i = 0; i < 6; i++)
+			for (int j = 0; j < 9; j++)
+				if ((squareMatrix[i][j][0].y <= -(1.5 * LENGTH - 1))
+						|| (squareMatrix[i][j][1].y <= -(1.5 * LENGTH - 1))
+						|| (squareMatrix[i][j][2].y <= -(1.5 * LENGTH - 1))
+						|| (squareMatrix[i][j][3].y <= -(1.5 * LENGTH))) {
+					rTransform(squareMatrix[i][j][0], 2, rdir);
+					rTransform(squareMatrix[i][j][1], 2, rdir);
+					rTransform(squareMatrix[i][j][2], 2, rdir);
+					rTransform(squareMatrix[i][j][3], 2, rdir);
+				}
+	}
+
+	public void frontTwist(int rdir) {
+		for (int i = 0; i < 6; i++)
+			for (int j = 0; j < 9; j++)
+				if ((squareMatrix[i][j][0].z >= 1.5 * LENGTH - 1)
+						|| (squareMatrix[i][j][1].z >= 1.5 * LENGTH - 1)
+						|| (squareMatrix[i][j][2].z >= 1.5 * LENGTH - 1)
+						|| (squareMatrix[i][j][3].z >= 1.5 * LENGTH - 1)) {
+					rTransform(squareMatrix[i][j][0], 3, rdir);
+					rTransform(squareMatrix[i][j][1], 3, rdir);
+					rTransform(squareMatrix[i][j][2], 3, rdir);
+					rTransform(squareMatrix[i][j][3], 3, rdir);
+				}
+	}
+
+	public void backTwist(int rdir) {
+		for (int i = 0; i < 6; i++)
+			for (int j = 0; j < 9; j++)
+				if ((squareMatrix[i][j][0].z <= -(1.5 * LENGTH - 1))
+						|| (squareMatrix[i][j][1].z <= -(1.5 * LENGTH - 1))
+						|| (squareMatrix[i][j][2].z <= -(1.5 * LENGTH - 1))
+						|| (squareMatrix[i][j][3].z <= -(1.5 * LENGTH - 1))) {
+					rTransform(squareMatrix[i][j][0], 3, rdir);
+					rTransform(squareMatrix[i][j][1], 3, rdir);
+					rTransform(squareMatrix[i][j][2], 3, rdir);
+					rTransform(squareMatrix[i][j][3], 3, rdir);
+				}
+	}
+
+	public void rotateCube(int axis, int rdir) {
+		// switch (axis) {
+		// case 1:
+		for (int i = 0; i < 6; i++) {
+			for (int j = 0; j < 9; j++) {
+				// if
+				// ((squareMatrix[i][j][0].x>=1.5*LENGTH-1)||(squareMatrix[i][j][1].x>=1.5*LENGTH-1)||(squareMatrix[i][j][2].x>=1.5*LENGTH-1)||(squareMatrix[i][j][3].x>=1.5*LENGTH-1))
+				// {
+				rTransform(squareMatrix[i][j][0], axis, rdir);
+				rTransform(squareMatrix[i][j][1], axis, rdir);
+				rTransform(squareMatrix[i][j][2], axis, rdir);
+				rTransform(squareMatrix[i][j][3], axis, rdir);
+				// }
+			}
+		}
+		// break;
+		// }
+	}
+
+	public void singleTwist() {
+		if (clock < (90 / DEG)) {
+			switch (faceTurn) {
+			case 11:
+				leftTwist(1);
+				clock++;
+				break;
+			case 12:
+				leftTwist(-1);
+				clock++;
+				break;
+			case 21:
+				leftTwist(-1);
+				rightTwist(-1);
+				rotateCube(1, 1);
+				clock++;
+				break;
+			case 22:
+				leftTwist(1);
+				rightTwist(1);
+				rotateCube(1, -1);
+				clock++;
+				break;
+			case 31:
+				rightTwist(1);
+				clock++;
+				break;
+			case 32:
+				rightTwist(-1);
+				clock++;
+				break;
+			case 41:
+				upTwist(-1);
+				clock++;
+				break;
+			case 42:
+				upTwist(1);
+				clock++;
+				break;
+			case 51:
+				upTwist(1);
+				bottomTwist(1);
+				rotateCube(2, -1);
+				clock++;
+				break;
+			case 52:
+				upTwist(-1);
+				bottomTwist(-1);
+				rotateCube(2, 1);
+				clock++;
+				break;
+			case 61:
+				bottomTwist(-1);
+				clock++;
+				break;
+			case 62:
+				bottomTwist(1);
+				clock++;
+				break;
+			case 71:
+				backTwist(1);
+				clock++;
+				break;
+			case 72:
+				backTwist(-1);
+				clock++;
+				break;
+			case 81:
+				backTwist(-1);
+				frontTwist(-1);
+				clock++;
+				break;
+			case 82:
+				backTwist(1);
+				frontTwist(1);
+				clock++;
+				break;
+			case 91:
+				frontTwist(1);
+				clock++;
+				break;
+			case 92:
+				frontTwist(-1);
+				clock++;
+				break;
+			case 101:
+				rotateCube(1, 1);
+				clock++;
+				break;
+			case 102:
+				rotateCube(1, -1);
+				clock++;
+				break;
+			case 103:
+				rotateCube(2, 1);
+				clock++;
+				break;
+			case 104:
+				rotateCube(2, -1);
+				clock++;
+				break;
+			}
+		} else {
+			clock = 0;
+			faceTurn = 0;
+		}
+	}
+}
---
+++ src/de/tub/mubix/main/Mubix.java
@@ -0,0 +1,285 @@
+package de.tub.mubix.main;
+
+import peasy.PeasyCam;
+import processing.core.PApplet;
+import SimpleOpenNI.SimpleOpenNI;
+import SimpleOpenNI.XnVSessionManager;
+import SimpleOpenNI.XnVSwipeDetector;
+
+@SuppressWarnings("serial")
+public class Mubix extends PApplet {
+	PeasyCam cam;
+
+	final int WINDOWX = 1024;
+	final int WINDOWY = 640;
+	final boolean enableOpenNI = false;
+
+	// int clock = 0;
+	// int ith = 0;
+	// int faceTurn = 0;
+	// boolean switch1 = false;
+	// String operation = new String();
+	// String solution = new String();
+
+	// PImage bkg_img;
+
+	public SimpleOpenNI context;
+	public XnVSessionManager sessionManager;
+	public XnVSwipeDetector swipeDetector;
+
+	Cube theCube;
+
+	@Override
+	public void setup() {
+
+		size(WINDOWX, WINDOWY, P3D);
+
+		/************************************************************
+		 ********************* OpenNI Stuff
+		 ************************************************************/
+
+		if (enableOpenNI) {
+			/*
+			 * Simple NI
+			 */
+			context = new SimpleOpenNI(this, SimpleOpenNI.RUN_MODE_MULTI_THREADED);
+			context.enableDepth();
+			context.enableUser(SimpleOpenNI.SKEL_PROFILE_ALL);
+			context.setMirror(true);
+
+			stroke(0, 0, 255);
+			strokeWeight(3);
+
+			// swipeDetector
+			sessionManager = new XnVSessionManager("RaiseHand");
+			sessionManager.SetTracker(context.getHandsGenerator());
+
+			swipeDetector = new XnVSwipeDetector();
+			swipeDetector.RegisterSwipeLeft(this);
+			swipeDetector.RegisterSwipeRight(this);
+			swipeDetector.RegisterSwipeUp(this);
+			swipeDetector.RegisterSwipeDown(this);
+			sessionManager.AddListener(swipeDetector);
+
+		}
+		// size(context.depthWidth(), context.depthHeight());
+
+		/************************************************************
+		 ********************* Cube Stuff
+		 ************************************************************/
+
+		cam = new PeasyCam(this, WINDOWX / 2, WINDOWY / 2, 0, 350);
+		cam.setMinimumDistance(50);
+		cam.setMaximumDistance(600);
+		cam.setActive(false); // deactivate mouse rotation
+		theCube = new Cube(this);
+
+		// bkg_img = loadImage("cube_black_1024_640.jpg");
+		// bkg_img.filter(OPAQUE);
+	}
+
+	@Override
+	public void draw() {
+		background(70, 130, 180);// For dynamically updating the background
+		if (enableOpenNI) {
+
+			context.update();
+			context.update(sessionManager);
+
+			// draw depthImageMap
+			image(context.depthImage(), 0, 0);
+
+			// draw the skeleton if it's available
+			if (context.isTrackingSkeleton(1))
+				drawSkeleton(1);
+		}
+		stroke(0, 0, 0);
+		translate(WINDOWX / 2, WINDOWY / 2);
+		rotateX(-0.4f);
+		rotateY(0.4f);
+		theCube.display();
+
+		theCube.singleTwist();
+		// image(bkg_img,-width/2, -height/2);
+		// println(frameRate);
+	}
+
+	// -----------------------------------------------------------------
+	// draw skeleton
+	// -----------------------------------------------------------------
+
+	// draw the skeleton with the selected joints
+	public void drawSkeleton(int userId) {
+		// to get the 3d joint data
+		/*
+		 * PVector jointPos = new PVector();
+		 * context.getJointPositionSkeleton(userId
+		 * ,SimpleOpenNI.SKEL_NECK,jointPos); println(jointPos);
+		 */
+
+		stroke(0, 0, 255);
+		strokeWeight(10);
+
+		context.drawLimb(userId, SimpleOpenNI.SKEL_HEAD, SimpleOpenNI.SKEL_NECK);
+
+		context.drawLimb(userId, SimpleOpenNI.SKEL_NECK,
+				SimpleOpenNI.SKEL_LEFT_SHOULDER);
+		context.drawLimb(userId, SimpleOpenNI.SKEL_LEFT_SHOULDER,
+				SimpleOpenNI.SKEL_LEFT_ELBOW);
+		context.drawLimb(userId, SimpleOpenNI.SKEL_LEFT_ELBOW,
+				SimpleOpenNI.SKEL_LEFT_HAND);
+
+		context.drawLimb(userId, SimpleOpenNI.SKEL_NECK,
+				SimpleOpenNI.SKEL_RIGHT_SHOULDER);
+		context.drawLimb(userId, SimpleOpenNI.SKEL_RIGHT_SHOULDER,
+				SimpleOpenNI.SKEL_RIGHT_ELBOW);
+		context.drawLimb(userId, SimpleOpenNI.SKEL_RIGHT_ELBOW,
+				SimpleOpenNI.SKEL_RIGHT_HAND);
+
+		context.drawLimb(userId, SimpleOpenNI.SKEL_LEFT_SHOULDER,
+				SimpleOpenNI.SKEL_TORSO);
+		context.drawLimb(userId, SimpleOpenNI.SKEL_RIGHT_SHOULDER,
+				SimpleOpenNI.SKEL_TORSO);
+
+		context.drawLimb(userId, SimpleOpenNI.SKEL_TORSO,
+				SimpleOpenNI.SKEL_LEFT_HIP);
+		context.drawLimb(userId, SimpleOpenNI.SKEL_LEFT_HIP,
+				SimpleOpenNI.SKEL_LEFT_KNEE);
+		context.drawLimb(userId, SimpleOpenNI.SKEL_LEFT_KNEE,
+				SimpleOpenNI.SKEL_LEFT_FOOT);
+
+		context.drawLimb(userId, SimpleOpenNI.SKEL_TORSO,
+				SimpleOpenNI.SKEL_RIGHT_HIP);
+		context.drawLimb(userId, SimpleOpenNI.SKEL_RIGHT_HIP,
+				SimpleOpenNI.SKEL_RIGHT_KNEE);
+		context.drawLimb(userId, SimpleOpenNI.SKEL_RIGHT_KNEE,
+				SimpleOpenNI.SKEL_RIGHT_FOOT);
+	}
+
+	// -----------------------------------------------------------------
+	// SimpleOpenNI events
+	// -----------------------------------------------------------------
+
+	public void onNewUser(int userId) {
+		println("onNewUser - userId: " + userId);
+		context.requestCalibrationSkeleton(userId, true);
+	}
+
+	public void onLostUser(int userId) {
+		println("onLostUser - userId: " + userId);
+	}
+
+	public void onStartCalibration(int userId) {
+		println("onStartCalibration - userId: " + userId);
+	}
+
+	public void onEndCalibration(int userId, boolean successfull) {
+		println("onEndCalibration - userId: " + userId + ", successfull: "
+				+ successfull);
+
+		if (successfull) {
+			println("  User calibrated !!!");
+			context.startTrackingSkeleton(userId);
+		} else {
+			println("  Failed to calibrate user !!!");
+		}
+	}
+
+	void onSwipeLeft(float velocity, float angle) {
+		println("SWIPELEFT " + velocity + " " + angle);
+	}
+
+	void onSwipeRight(float velocity, float angle) {
+		println("SWIPERIGHT " + velocity + " " + angle);
+
+	}
+
+	void onSwipeUp(float velocity, float angle) {
+		println("SWIPEUP " + velocity + " " + angle);
+	}
+
+	void onSwipeDown(float velocity, float angle) {
+		println("SWIPEDOWN " + velocity + " " + angle);
+	}
+
+	// -----------------------------------------------------------------
+	// Cube Stuff
+	// -----------------------------------------------------------------
+
+	@Override
+	public void keyPressed() {
+		if (theCube.clock == 0) {
+			if (key == '1') {
+				theCube.faceTurn = 11;
+			}
+			if (key == '2') {
+				theCube.faceTurn = 21;
+			}
+			if (key == '3') {
+				theCube.faceTurn = 31;
+			}
+			if (key == '4') {
+				theCube.faceTurn = 41;
+			}
+			if (key == '5') {
+				theCube.faceTurn = 51;
+			}
+			if (key == '6') {
+				theCube.faceTurn = 61;
+			}
+			if (key == '6') {
+				theCube.faceTurn = 61;
+			}
+			if (key == '7') {
+				theCube.faceTurn = 71;
+			}
+			if (key == '8') {
+				theCube.faceTurn = 81;
+			}
+			if (key == '9') {
+				theCube.faceTurn = 91;
+			}
+			if ((key == 'q') || (key == 'Q')) {
+				theCube.faceTurn = 12;
+			}
+			if ((key == 'w') || (key == 'W')) {
+				theCube.faceTurn = 22;
+			}
+			if ((key == 'e') || (key == 'E')) {
+				theCube.faceTurn = 32;
+			}
+			if ((key == 'r') || (key == 'R')) {
+				theCube.faceTurn = 42;
+			}
+			if ((key == 't') || (key == 'T')) {
+				theCube.faceTurn = 52;
+			}
+			if ((key == 'z') || (key == 'Z')) {
+				theCube.faceTurn = 62;
+			}
+			if ((key == 'u') || (key == 'U')) {
+				theCube.faceTurn = 72;
+			}
+			if ((key == 'i') || (key == 'I')) {
+				theCube.faceTurn = 82;
+			}
+			if ((key == 'o') || (key == 'O')) {
+				theCube.faceTurn = 92;
+			}
+			if (key == CODED) {
+				if (keyCode == UP) {
+					theCube.faceTurn = 101;
+				}
+				if (keyCode == DOWN) {
+					theCube.faceTurn = 102;
+				}
+				if (keyCode == LEFT) {
+					theCube.faceTurn = 103;
+				}
+				if (keyCode == RIGHT) {
+					theCube.faceTurn = 104;
+				}
+			}
+		}
+	}
+}
